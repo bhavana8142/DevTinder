46e4eeca74249cbd3314e917f58f92886705dc47
bhavana-battu <battubhavanacse@gmail.com>
2026-08-22 00:38:36 +0530

    get,post,delte

--- src/app.js
+++ src/app.js
@@ -1,14 +1,22 @@
 const express = require("express")
 
 const app = express()
-
-app.use("/test", (req, res) => {
-    res.send("Hello DevTinder")
+/*
+        app.use("/user", (req, res) => {
+    res.send('use function')
 })
 
-app.use('/', (req, res) => {
-    res.send("Hello")
+*/
+app.get("/user", (req, res) => {
+    res.send({ firstName: "Bhavana", lastName: "Battu" })
 })
+app.post("/user", (req, res) => {
+    res.send("data Saved successfully")
+})
+app.delete("/user", (req, res) => {
+    res.send("Data deleted successfully")
+})
+
 
 app.listen(3000, () => {
     console.log("server is started successfully")
